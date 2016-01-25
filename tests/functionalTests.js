bba94d27fc77bf073aaf34e8aa8eed1c1a692b3d
Alex Booker <alexbooker@fastmail.im>
2016-01-25 14:02:15 +0000

    feat(cli): Add "--template" and "--scheme" arguments
    Closes #5

--- tests/functionalTests.js
+++ tests/functionalTests.js
@@ -12,10 +12,29 @@ test('given "help" arg program returns man page', async function(t) {
   t.ok(output.match(/man/i));
 });
 
-test('given \"theme\" and \"scheme\" names program writes output file', async function(t) {
+test('given \"theme\" and \"scheme\" short names program writes output file', async function(t) {
   const schemeName = 'oceanicnext';
   const templName = 'i3wm';
   const args = ['-s', schemeName, '-t', templName];
+
+  await execa(cmd, args);
+
+  try {
+    const actual =
+      await fs.readFile(`output/${templName}/${schemeName}`, 'utf8');
+    t.ok(/set \$base00 1B2B34/.test(actual));
+  } catch (err) {
+    t.fail(err);
+  } finally {
+    await rimraf('output');
+  }
+})
+
+
+test('given \"theme\" and \"scheme\" long names program writes output file', async function(t) {
+  const schemeName = 'oceanicnext';
+  const templName = 'i3wm';
+  const args = ['--scheme', schemeName, '--template', templName];
 
   await execa(cmd, args);
 
