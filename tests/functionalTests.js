fd4011c01cce7ac9a6a3e3c93c4cfb7f4848ddbb
Alex Booker <alexbooker@fastmail.im>
2016-02-04 00:41:41 +0000

    test(cli): Add tests for path support
    Add tests for path support, which was added in 76ede9ca647c4efb68c5a88562e770366645422c by @aloisdg

--- tests/functionalTests.js
+++ tests/functionalTests.js
@@ -7,7 +7,7 @@ import countLines from 'line-count';
 
 const command = '../dist/cli.js';
 
-test('non-existent template should cause error', async function (t) {
+test('non-existent template name should cause error', async function (t) {
   const templ = 'foo';
   const commandArguments = ['-s', 'monokai', '-t', templ];
   const {stderr: actual} = await execute(command, commandArguments);
@@ -29,7 +29,7 @@ See \'base16-builder ls-templates\' for a list of available templates.`;
   }
 });
 
-test('non-existent scheme should cause error', async function (t) {
+test('non-existent scheme name should cause error', async function (t) {
   const scheme = 'bar';
   const commandArguments = ['-s', scheme, '-t', 'i3wm'];
   const {stderr: actual} = await execute(command, commandArguments);
@@ -84,7 +84,7 @@ test('with alias help arguments should cause help to be output', async function 
   t.ok(actual.match(/Example/));
 });
 
-test('valid arguments cause output file to be written to console', async function (t) {
+test('valid template and scheme cause output file to be written to console', async function (t) {
   const scheme = 'oceanicnext';
   const templ = 'i3wm';
   const commandArguments = ['-s', scheme, '-t', templ];
@@ -94,7 +94,7 @@ test('valid arguments cause output file to be written to console', async functio
   t.ok(/set \$base00 1B2B34/.test(actual));
 });
 
-test('with aliases, valid arguments cause output file to be written to console', async function (t) {
+test('with aliases, valid template and scheme cause output file to be written to console', async function (t) {
   const scheme = 'oceanicnext';
   const templ = 'i3wm';
   const commandArguments = ['--scheme', scheme, '--template', templ];
@@ -146,4 +146,64 @@ test('template list is sorted alphabetically', async function (t) {
   const expected = actual.split('\n').sort().join('\n');
 
   t.is(actual, expected);
+});
+
+test('valid template and scheme paths cause output file to be written to console', async function (t) {
+  const schemePath = '../db/schemes/oceanicnext.yml';
+  const templPath = '../db/templates/i3wm.nunjucks';
+  const commandArguments = ['--scheme', schemePath, '--template', templPath];
+
+  const {stdout: actual} = await execute(command, commandArguments);
+
+  t.ok(/set \$base00 1B2B34/.test(actual));
+});
+
+test('with aliases, valid template and scheme paths cause output file to be written to console', async function (t) {
+  const schemePath = '../db/schemes/oceanicnext.yml';
+  const templPath = '../db/templates/i3wm.nunjucks';
+  const commandArguments = ['-s', schemePath, '-t', templPath];
+
+  const {stdout: actual} = await execute(command, commandArguments);
+
+  t.ok(/set \$base00 1B2B34/.test(actual));
+});
+
+test('non-existent template path should cause error', async function (t) {
+  const templPath = 'foo/i3wm.nunjucks';
+  const commandArguments = ['--scheme', 'monokai', '--template', templPath];
+  const {stderr: actual} = await execute(command, commandArguments);
+
+  const expected = `fatal: Could not find a template called '${templPath}'.
+See \'base16-builder ls-templates\' for a list of available templates.`;
+  t.is(actual, expected);
+});
+
+test('non-existent scheme path should cause error', async function (t) {
+  const schemePath = 'foo/oceanicnext.yml';
+  const commandArguments = ['-s', schemePath, '-t', 'i3wm'];
+  const {stderr: actual} = await execute(command, commandArguments);
+
+  const expected = `fatal: Could not find a scheme called '${schemePath}'.
+See \'base16-builder ls-schemes\' for a list of available schemes.`;
+  t.is(actual, expected);
+});
+
+test('valid template name and scheme path cause output file to be written to console', async function (t) {
+  const schemePath = '../db/schemes/oceanicnext.yml';
+  const templ = 'i3wm';
+  const commandArguments = ['--scheme', schemePath, '--template', templ];
+
+  const {stdout: actual} = await execute(command, commandArguments);
+
+  t.ok(/set \$base00 1B2B34/.test(actual));
+});
+
+test('valid template path and scheme name cause output file to be written to console', async function (t) {
+  const scheme = 'oceanicnext';
+  const templPath = '../db/templates/i3wm.nunjucks';
+  const commandArguments = ['--scheme', scheme, '--template', templPath];
+
+  const {stdout: actual} = await execute(command, commandArguments);
+
+  t.ok(/set \$base00 1B2B34/.test(actual));
 });
