0d3f35e6e179277a6b3b26c65b681e9a1db1b707
Alex Booker <alexbooker@fastmail.im>
2016-02-09 18:43:06 +0000

    feat(cli): Update help page

--- tests/functionalTests.js
+++ tests/functionalTests.js
@@ -9,7 +9,6 @@ test('help arguments should cause help to be output', async function (t) {
   const {stdout: actual} = await execute(command, ['--help']);
 
   t.ok(actual.match(/Usage/));
-  t.ok(actual.match(/Commands/));
   t.ok(actual.match(/Options/));
   t.ok(actual.match(/Example/));
 });
@@ -18,7 +17,6 @@ test('with alias help arguments should cause help to be output', async function 
   const {stdout: actual} = await execute(command, ['-h']);
 
   t.ok(actual.match(/Usage/));
-  t.ok(actual.match(/Commands/));
   t.ok(actual.match(/Options/));
   t.ok(actual.match(/Example/));
 });
