54c593bbe7bda28c056226c60d5119672708d1ac
Alex Booker <alexbooker@fastmail.im>
2016-03-15 01:48:08 +0000

    refactor(cli): Adopt standard style

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,9 +1,12 @@
 module.exports = {
   parser: 'babel-eslint',
-  extends: 'google',
+  extends: 'standard',
+  plugins: [
+    'standard'
+  ],
   rules: {
     'no-var': 2,
     'max-len': [2, 120, 2],
     'require-jsdoc': 0
   }
-};
+}
--- util/schemesPreviewGenerator.js
+++ util/schemesPreviewGenerator.js
@@ -1,30 +1,30 @@
-const fs = require('fs-promise');
-const path = require('path');
-const yaml = require('js-yaml');
-const Promise = require('bluebird');
-const ejs = require('ejs');
+const fs = require('fs-promise')
+const path = require('path')
+const yaml = require('js-yaml')
+const Promise = require('bluebird')
+const ejs = require('ejs')
 
-const schemesDir = path.join(__dirname, '../db/schemes');
+const schemesDir = path.join(__dirname, '../db/schemes')
 
 fs
   .readdir(schemesDir)
-  .then(function(schemeFileNames) {
-    const promises = [];
-    schemeFileNames.forEach(function(schemeFileName) {
-      promises.push(fs.readFile(path.join(schemesDir, schemeFileName), 'utf-8'));
-    });
-    return Promise.all(promises).then(function(yamlSchemes) {
-      yamlSchemes = yamlSchemes.map(yamlScheme => yaml.load(yamlScheme));
+  .then(function (schemeFileNames) {
+    const promises = []
+    schemeFileNames.forEach(function (schemeFileName) {
+      promises.push(fs.readFile(path.join(schemesDir, schemeFileName), 'utf-8'))
+    })
+    return Promise.all(promises).then(function (yamlSchemes) {
+      yamlSchemes = yamlSchemes.map(yamlScheme => yaml.load(yamlScheme))
       return fs
         .readFile(path.join(__dirname, './schemesPreview.ejs'), 'utf-8')
-        .then(function(templ) {
+        .then(function (templ) {
           const preview = ejs.render(templ, {
             schemes: yamlSchemes
-          });
-          return fs.writeFile(path.join(__dirname, '../dist/index.html'), preview);
-        });
-    });
+          })
+          return fs.writeFile(path.join(__dirname, '../dist/index.html'), preview)
+        })
+    })
   })
-  .catch(function(err) {
+  .catch(function (err) {
     console.error('err', err)
-  });
+  })
